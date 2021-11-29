e0e0e6e88cc837af84a61d199e8dfd8ab4a44838
Juliyana <juliyana.pavlova01@gmail.com>
2021-11-29 17:27:44 +0200

    GitHub Test

--- src/main/java/TestGit/TestGit.java
+++ src/main/java/TestGit/TestGit.java
@@ -1,10 +1,23 @@
 package TestGit;
 
+import java.util.Scanner;
+
 public class TestGit
 {
-    public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Hello Git");
+    public static void main(String[] args)
+    {
+        Scanner consoleInput = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = consoleInput.nextLine();
 
+        System.out.println("Please enter your age: ");
+        int age = consoleInput.nextInt();
+
+        if (age<18)
+        {
+            System.out.println(name + " can't buy alcohol");
+        }else{
+            System.out.println(name + " can buy alcohol");
+        }
     }
 }
