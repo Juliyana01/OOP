ad18f8a0842ddbdf2588fc85c654b31aeb00016d
Juliyana <juliyana.pavlova01@gmail.com>
2021-12-04 15:02:49 +0200

    GitHub Test

---
+++ src/main/java/Test/Test.java
@@ -0,0 +1,15 @@
+package Test;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        compare(3,2);
+    }
+
+    public static void compare(int a, int b)
+    {
+        System.out.println(a > b ? "First number is grater" : "Might be equal");
+    }
+
+}
